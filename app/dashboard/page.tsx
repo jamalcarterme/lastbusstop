dd6245e630e3718efa4112683d736cbc2b8dc80a
jamalcarterme <jamalcarterme@gmail.com>
2026-09-20 23:57:55 +0100

    Delete app directory

--- app/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-import DashboardClient from "./DashboardClient";
-
-export const metadata = { title: "My Account - Last Bus Stop Ministry" };
-
-export default function Dashboard() {
-  return <DashboardClient />;
-}
